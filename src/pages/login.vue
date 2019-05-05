be6430f8d6d9398dc3c906df9c8cf3837933c31c
code-admin <1157452915@qq.com>
2019-05-05 15:51:01 +0800

    Update login page

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,214 +1,244 @@
 <template>
-  <div class="user-login">
+<div class="user-login">
     <div class="formContainer">
-      <h4 class="formTitle">登 录</h4>
-      <el-form :model="loginForm" class="formItems">
-        <el-form-item class="next-row formItem">
-          <span class="next-col formItemCol">
+        <h4 class="formTitle">登 录</h4>
+        <el-form :model="loginForm" class="formItems" ref="loginForm" :rules="rules">
+            <el-form-item class="next-row formItem" prop="username">
+                <span class="next-col formItemCol">
             <el-input
-              name="username"
-              prefix-icon="iconfont-user"
+              autofocus
+              clearable
+              prefix-icon="el-icon-user"
               v-model="loginForm.username"
               placeholder="用户名"
-              data-vv-as="用户名"
+              @keyup.enter.native="checkLogin('loginForm')"
             ></el-input>
           </span>
-        </el-form-item>
+            </el-form-item>
 
-        <el-form-item class="next-row formItem">
-          <span class="next-col formItemCol">
+            <el-form-item class="next-row formItem" prop="password">
+                <span class="next-col formItemCol">
             <el-input
-              name="password"
               type="password"
-              prefix-icon="iconfont-password"
+              clearable
+              prefix-icon="el-icon-lock"
               v-model="loginForm.password"
               placeholder="密码"
-              data-vv-as="密码"
+              @keyup.enter.native="checkLogin('loginForm')"
             ></el-input>
           </span>
-        </el-form-item>
+            </el-form-item>
 
-        <el-form-item class="next-row formItem">
-          <span class="next-col">
+            <el-form-item class="next-row formItem">
+                <span class="next-col">
             <el-checkbox v-model="loginForm.remember">记住账号</el-checkbox>
           </span>
-        </el-form-item>
+            </el-form-item>
 
-        <div class="next-row formItem">
-          <el-button
-            type="primary"
-            v-loading.fullscreen.lock="fullscreenLoading"
-            :loading="fullscreenLoading"
-            @click="submitForm"
-            class="submitBtn"
-          >登录</el-button>
-        </div>
-      </el-form>
+            <div class="next-row formItem">
+                <el-button type="primary" v-loading.fullscreen.lock="fullscreenLoading" :loading="fullscreenLoading" @click="checkLogin('loginForm')" class="submitBtn">登录</el-button>
+            </div>
+        </el-form>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: "Login",
-  data() {
-    return {
-      loadingInstance: null,
-      fullscreenLoading: false,
-      loginForm: {
-        username: "",
-        password: "",
-        remember: false
-      }
-    };
-  },
-  methods: {
-    submitForm() {
-      this.fullscreenLoading = true;
-      let api = "/login/login";
-      let params = {
-        username: this.loginForm.username,
-        password: this.loginForm.password
-      };
-      this.$fetch
-        .post(api, params)
-        .then(res => {
-          if (res.code === 10000) {
-            this.$fetch.config.headers["QT-USER-TOKEN"] = res.data.authToken;
-            window.sessionStorage.setItem(
-              "QT-USER-TOKEN",
-              res.data && res.data.authToken
-            );
-            this.fullscreenLoading = false;
-            this.$router.push("app");
-          } else {
-            this.fullscreenLoading = false;
-            this.$message.error(res.message);
-          }
-        })
-        .catch(err => {
-          this.$message.error(err.message);
-          this.fullscreenLoading = false;
-        });
+    name: "Login",
+    data() {
+        return {
+            fullscreenLoading: false,
+            loginForm: {
+                username: "",
+                password: "",
+                remember: false
+            },
+            rules: {
+                username: [{
+                        required: true,
+                        message: '请输入登录名',
+                        trigger: 'blur'
+                    },
+                    {
+                        min: 1,
+                        max: 20,
+                        message: '登录名最大长度在为20个字符',
+                        trigger: 'blur'
+                    }
+                ],
+                password: [{
+                        required: true,
+                        message: '请输入登录密码',
+                        trigger: 'blur'
+                    },
+                    {
+                        min: 6,
+                        max: 20,
+                        message: '长度在6到 20个字符',
+                        trigger: 'blur'
+                    }
+                ]
+            }
+        };
+    },
+    methods: {
+        checkLogin(loginForm) {
+            this.$refs[loginForm].validate(valid => {
+                if (valid) {
+                    this.submitForm();
+                } else {
+                    this.$message.error('登录验证失败');
+                    return false;
+                }
+            })
+        },
+        submitForm() {
+            this.fullscreenLoading = true;
+            let api = "/login/login";
+            let params = {
+                username: this.loginForm.username,
+                password: this.loginForm.password
+            };
+            this.$fetch
+                .post(api, params)
+                .then(res => {
+                    if (res.code === 10000) {
+                        this.$fetch.config.headers["QT-USER-TOKEN"] = res.data.authToken;
+                        window.sessionStorage.setItem(
+                            "QT-USER-TOKEN",
+                            res.data && res.data.authToken
+                        );
+                        this.fullscreenLoading = false;
+                        this.$router.push("app");
+                    } else {
+                        this.fullscreenLoading = false;
+                        this.$message.error(res.message);
+                    }
+                })
+                .catch(err => {
+                    this.$message.error(err.message);
+                    this.fullscreenLoading = false;
+                });
+        }
     }
-  },
-  beforeDestroy() {
-    this.loadingInstance && this.loadingInstance.close();
-  }
 };
 </script>
 
 <style lang="scss" scoped>
-
 .formContainer {
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  max-width: 338px;
-  margin: 0 auto;
-  padding: 20px;
-  border-radius: 8px;
-  background: rgba(255,255,255,.08);
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    max-width: 338px;
+    margin: 0 auto;
+    padding: 20px;
+    border-radius: 8px;
+    background: rgba(255, 255, 255, .08);
 }
 
 .formItem {
-  position: relative;
-  margin-bottom: 25px;
-  flex-direction: column;
+    position: relative;
+    margin-bottom: 25px;
+    flex-direction: column;
 }
 
 .formItemCol {
-  position: relative;
-  margin-bottom: 5px;
+    position: relative;
+    margin-bottom: 5px;
 }
 
 [class*="iconfont"] {
-  font-size: 16px !important;
+    font-size: 16px !important;
 }
 
 .formTitle {
-  text-align: center;
-  margin: 0 0 20px;
-  font-size: 20px;
-  color: rgba(255, 255, 255, 0.85);
-  font-weight: bold;
+    text-align: center;
+    margin: 0 0 20px;
+    font-size: 20px;
+    color: rgba(255, 255, 255, 0.85);
+    font-weight: bold;
 }
 
 .inputIcon {
-  position: absolute;
-  left: 12px;
-  top: 50%;
-  transform: translateY(-50%);
-  color: #999;
+    position: absolute;
+    left: 12px;
+    top: 50%;
+    transform: translateY(-50%);
+    color: #999;
 }
 
 .submitBtn {
-  height: 40px;
-  font-size: 16px;
-  border-radius: 4px;
+    height: 40px;
+    font-size: 16px;
+    border-radius: 4px;
 }
 
 .checkbox {
-  margin-left: 5px;
+    margin-left: 5px;
 }
 
 .tips {
-  justify-content: center;
+    justify-content: center;
 }
 
 .tips-text {
-  color: #999;
-  margin-top: 3px;
-  text-decoration: none;
-  font-size: 13px;
+    color: #999;
+    margin-top: 3px;
+    text-decoration: none;
+    font-size: 13px;
 }
 
 .line {
-  color: #dcd6d6;
-  margin: 0 8px;
+    color: #dcd6d6;
+    margin: 0 8px;
 }
 
 .next-row {
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
-  display: -webkit-box;
-  display: -webkit-flex;
-  display: -ms-flexbox;
-  display: flex;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
 }
 
 .next-col {
-  -webkit-box-flex: 1;
-  -webkit-flex: 1;
-  -ms-flex: 1;
-  flex: 1;
+    -webkit-box-flex: 1;
+    -webkit-flex: 1;
+    -ms-flex: 1;
+    flex: 1;
 }
 
 .next-input.next-input-single {
-  width: 368px;
-  height: 38px;
-  line-height: 38px;
+    width: 368px;
+    height: 38px;
+    line-height: 38px;
 
-  input {
-    padding-left: 32px;
-    font-size: 13px;
-  }
+    input {
+        padding-left: 32px;
+        font-size: 13px;
+    }
 }
 
 .el-checkbox {
-  color: #999;
-  font-size: 13px;
+    color: #999;
+    font-size: 13px;
 }
+
 .el-input /deep/ input {
-  background-color: #e8f0fe;
-  &::-webkit-input-placeholder{
-    color:#5e6064;
-  }
-  &::-moz-placeholder{
-    color:#5e6064;
-  }
-  &:-ms-input-placeholder{
-    color:#5e6064;
-  }
+    background-color: #e8f0fe;
+
+    &::-webkit-input-placeholder {
+        color: #5e6064;
+    }
+
+    &::-moz-placeholder {
+        color: #5e6064;
+    }
+
+    &:-ms-input-placeholder {
+        color: #5e6064;
+    }
 }
 </style>
